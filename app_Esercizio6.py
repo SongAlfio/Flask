85fd8642cae7222c9cb1485029303b814fb8ec10
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2022-03-24 10:48:06 +0000

    Aggiornamento

--- app_Esercizio6.py
+++ app_Esercizio6.py
@@ -14,9 +14,9 @@ import matplotlib.pyplot as plt
 
 
 
-Quartiere = gpd.read_file('/workspace/Flask/ds964_nil_wm (1).zip').to_crs(epsg=3857)
-Comuni = gpd.read_file('/workspace/Flask/Comuni.zip').to_crs(epsg=3857)
-Fontanelle = gpd.read_file('/workspace/Flask/Fontanelle.zip').to_crs(epsg=3857)
+Quartiere = gpd.read_file('/workspace/Flask/ds964_nil_wm (1).zip')
+Comuni = gpd.read_file('/workspace/Flask/Comuni.zip')
+Fontanelle = gpd.read_file('/workspace/Flask/Fontanelle.zip')
 
 
 @app.route('/', methods=['GET'])
@@ -59,8 +59,14 @@ def Cerca():
     return Response(output.getvalue(), mimetype='image/png')
 
 @app.route('/visualizza', methods=['GET'])
-def visualizza():
-    return render_template('visualizza_Es6.html')
+def Visualizza_dati():
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    Quartiere.to_crs(epsg=3857).plot(ax=ax, alpha=0.5,edgecolor='k')
+    contextily.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 @app.route('/ricerca', methods=['GET'])
 def ricerca():
@@ -74,18 +80,10 @@ def scelta():
 @app.route('/fontanelle', methods=['GET'])
 def fontanelle():
     quartiere = Quartiere['NIL'].drop_duplicates().to_list()
+    quartiere.sort()
     return render_template('fontanelle_Es6.html',quartiere=quartiere)
 
-@app.route('/dati_visualizza', methods=['GET'])
-def visualizza_dati():
-    fig, ax = plt.subplots(figsize = (12,8))
 
-    Quartiere.to_crs(epsg=3857).plot(ax=ax, alpha=0.5,edgecolor='k')
-    contextily.add_basemap(ax=ax)
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-    
 @app.route('/dati_ricerca', methods=['GET'])
 def Ricerca_dati():
     Ricerca = request.args['ricerca']
@@ -118,14 +116,14 @@ def Sceglie_dati():
 def Fontanelle_dati():
     Scelta = request.args['Scelta']
     Trovato = Quartiere[Quartiere['NIL'] == Scelta]
-    Fontanelle1 = gpd.sjoin( Fontanelle, Trovato, how = 'left', op = 'intersects')
-    Fontanelle2 = Fontanelle1[Fontanelle1.intersects(Trovato)].to_crs(epsg=3857)
+    Fontanelle2 = Fontanelle[Fontanelle.within(Trovato.unary_union)]
     fig, ax = plt.subplots(figsize = (12,8))
+    Trovato.to_crs(epsg=3857).plot(ax=ax,alpha=0.5,facecolor='g')
 
-    Fontanelle2.to_crs(epsg=3857).plot(ax=ax)
     contextily.add_basemap(ax=ax)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
