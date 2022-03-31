3a682997974ce484f5595a18f5c0f192474cd772
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2022-03-31 09:51:09 +0000

    Aggiornamento

--- Correzione_Verifica_A.py
+++ Correzione_Verifica_A.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, make_response, url_for, Response,request
+from flask import Flask, render_template, send_file, make_response, url_for, Response, request, redirect
 app = Flask(__name__)
 import pandas as pd
 import io
@@ -11,17 +11,41 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 # 1. Avere il numero di stazioni per ogni municipio (in ordine crescente sul numero del municipio) e il grafico corrispondente
-Stazioni = pd.read_csv('/workspace/Flask/templates/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv',sep=";")
+Stazioni = pd.read_csv('/workspace/Flask/templates/Verifica_A/Radio.csv',sep=";")
 Quartieri = gpd.read_file('/workspace/Flask/Quartieri.zip')
+risultato = Stazioni.groupby('MUNICIPIO')['OPERATORE'].count().reset_index()
 @app.route('/', methods=['GET'])
 def homepage():
-    return render_template("home.html")
+    return render_template("Verifica_A/home1.html")
 
+@app.route('/selezione', methods=['GET'])
+def selezione():
+    scelta = request.args['scelta']
+    if scelta == 'Esercizio1':
+        return redirect(url_for('numero'))#重新定向
+    elif scelta == 'Esercizio2':
+        return redirect(url_for('input'))
+    else:
+        return redirect(url_for('dropdown'))
 @app.route('/numero', methods=['GET'])
 def numero():
 #numero stazioni per ogni municipio
-    risultato = Stazioni.groupby('MUNICIPIO')['OPERATORE'].count().reset_index()
-    return render_template("Elenco.html",risultato = risultato.to_html())
+    
+    return render_template("Verifica_A/Elenco.html",risultato = risultato.to_html())
+
+@app.route('/grafico', methods=['GET'])
+def grafico():
+#Costruzione del grafico
+    fig, ax = plt.subplots(figsize = (6,4))
+
+    x = risultato.MUNICIPIO
+    y = risultato.OPERATORE
+
+    ax.bar(x, y, color = "#304C89")
+#visualizzazione del grafico
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
