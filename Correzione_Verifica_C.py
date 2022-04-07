a6a766457fa26bbcecfb48a0f1b5070e9ecbc4d1
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2022-04-07 09:46:49 +0000

    Aggiornamento

--- Correzione_Verifica_C.py
+++ Correzione_Verifica_C.py
@@ -27,7 +27,7 @@ def selezione():
     elif scelta == 'Esercizio2':
         return redirect(url_for('input1'))
     else:
-        return redirect(url_for('dropdown'))
+        return redirect(url_for('scelta'))
 
 
 @app.route('/input', methods=['GET'])
@@ -39,24 +39,40 @@ def elenco():
     L_Minima = request.args['elenco1']
     L_Massima = request.args['elenco2']
     Bus_Tram1 = Bus_Tram[(Bus_Tram['lung_km'] < L_Massima) & (Bus_Tram['lung_km'] > L_Minima)]
-    return render_template("Correzione_Verifica_C/Elenco.html",Bus_Tram = Bus_Tram1['nome'].to_list())
+    Bus_Tram1 = Bus_Tram1['linea'].drop_duplicates().to_list()
+    Bus_Tram1.sort(key=int)
+    return render_template("Correzione_Verifica_C/Elenco.html",Bus_Tram = Bus_Tram1)
 
 @app.route('/input1', methods=['GET'])
 def input1():
     return render_template("Correzione_Verifica_C/Input1.html")
 
-@app.route('/mappa', methods=['GET'])
-def mappa():
+@app.route('/elenco1', methods=['GET'])
+def elenco1():
     Quartiere_Cercato = request.args['quartiere']
     Quartiere_Trovato = Quartieri[Quartieri['NIL'].str.contains(Quartiere_Cercato)]
     Bus_Tram1 = Bus_Tram[Bus_Tram.intersects(Quartiere_Trovato.unary_union)]
-    fig, ax = plt.subplots(figsize = (12,8))
-    Bus_Tram1.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
-    contextily.add_basemap(ax=ax)
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
+    Bus_Tram1 = Bus_Tram1['linea'].drop_duplicates().to_list()
+    Bus_Tram1.sort(key=int)
+    return render_template("Correzione_Verifica_C/Elenco.html",Bus_Tram = Bus_Tram1)
 
+@app.route('/scelta', methods=['GET'])
+def scelta():
+    Bus_Tram1 = Bus_Tram['linea'].drop_duplicates().to_list()
+    Bus_Tram1.sort(key=int)
+    return render_template("Correzione_Verifica_C/Scelta.html",linea = Bus_Tram1)
+
+@app.route('/linea', methods=['GET'])
+def Plot():
+        Linea_Scelto = request.args['linee']
+        Plot = Bus_Tram[Bus_Tram['linea']==Linea_Scelto]
+        fig, ax = plt.subplots(figsize = (12,8))
+
+        Plot.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
+        contextily.add_basemap(ax=ax)
+        output = io.BytesIO()
+        FigureCanvas(fig).print_png(output)
+        return Response(output.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
